5b9685277e5199367550ccc810e94aa45b64a9c4
azdrogov <azdrogov@naumen.ru>
2022-01-20 16:41:51 +0500

    STB-4 Сделать читаемость сценария

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,21 +2,28 @@ import cats.effect._
 import modules.Bot
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import cats.effect.implicits._
-import cats.syntax._
+import domain.Scenario
+import io.circe.yaml.parser
 
+import scala.io.BufferedSource
 
 
 object Main extends IOApp {
 
   implicit val logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
+  val res: IO[BufferedSource] = IO(scala.io.Source.fromFile("/home/azdrogov/scalaTelegramBot/test.yaml"))
+
   override def run(args: List[String]): IO[ExitCode] = {
     config.loader[IO].flatMap { cfg =>
       Logger[IO].info(s"Loaded config $cfg")
-      for {
-        _ <- new Bot[IO](cfg.token.value.value.value).startPolling()
-      } yield ExitCode.Success
+
+      Resource.fromAutoCloseable(res).use { source =>
+        for {
+          scenario <- parser.parse(source.mkString).flatMap(_.as[Scenario])
+          _ <- new Bot[IO](cfg.token.value.value.value).startPolling()
+        } yield ExitCode.Success
+      }
     }
   }
 }
